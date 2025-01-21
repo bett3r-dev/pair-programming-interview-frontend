84053b0cabb2d14ac43812b83e1f4ed1288af6ca
Tomas Ruiz <tomasruizr@gmail.com>
2025-01-20 23:31:57 -0300

    fix: contexts useBackend cleanup

--- src/contexts/useBackend/index.tsx
+++ src/contexts/useBackend/index.tsx
@@ -1,13 +1,11 @@
 import { useState, useContext, createContext, ReactNode, useEffect } from 'react';
 import { create as createBackend } from '@bett3r-dev/pair-programming-interview-client-library';
-import useAuth, { AuthContextType } from '../useAuth';
 
 type Props = { children: ReactNode }
 export type BackendType = any;
 export type BackendContextType = { backend: ReturnType<typeof createBackend>, setBackend: (value: BackendType) => void }
 
 export const backendContext = createContext<BackendContextType | null>(null);
-
 
 export function ProvideBackend({ children }: Props) {
     const _backend = useProvideBackend() as BackendContextType;
@@ -18,23 +16,14 @@ export default function useBackend() {
     return useContext(backendContext);
 }
 
-//TODO: Fake, reemplazar
-const createBackend = ({ baseUrl, token }: { baseUrl: string, token: string | undefined }) => {
-    return {
-        baseUrl,
-        token
-    }
-}
-
 function useProvideBackend() {
     const [backend, setBackend] = useState<ReturnType<typeof createBackend> | null>();
-    const { user } = useAuth() as AuthContextType;
     useEffect(() => {
-        setBackend(createBackend({ baseUrl: import.meta.env.VITE_REACT_APP_BACKEND_URL || '', token: user?.token }));
+        setBackend(createBackend({ baseUrl: import.meta.env.VITE_REACT_APP_BACKEND_URL || '' }));
         return () => {
             // backend.close();
         };
-    }, [user]);
+    }, []);
 
     return {
         backend,
